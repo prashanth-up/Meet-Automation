08d5d6293f14e582c01228dd13814204c53915d5
prashanth-up <ujprashant@gmail.com>
2020-07-31 18:06:29 +0530

    User has an option to comment info in meeting.

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import pyautogui as pag
 
 url = str(input('Enter the meeting id :'))
 meeting_time = int(input('Enter total minutes of the meeting: '))
+comment_ask = input('Type yes/no to print your attendance info in comments :')
+# name, regNo, classSec = '','',''
 
+if (comment_ask.lower() == 'yes' or 'y' or 'Y'):
+    print("Please Enter the following details to be shown in comments")
+    name = input('Please enter your name :')
+    regNo = input('Enter your register number :')
+    classSec = input('Enter your Year and Section :')
+    foo = True
+else:
+    foo = False
 
 # Windows
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
@@ -29,30 +39,33 @@ def meeting_join():
     print('Meeting Started')
     time.sleep(2)
 
-def comment():
+def comment(name,regNo,classSec):
     time.sleep(15)
     pag.press('tab')
-    time.sleep(.5)
+    time.sleep(1)
+    pag.press('tab')
+    time.sleep(1)
     pag.press('tab')
     time.sleep(1)
     pag.press('enter')
 
     time.sleep(4)
 
-    pag.write("Prashanth Umapathy", interval=0.1)
+    pag.write(name, interval=0.1)
     time.sleep(0.5)
     pag.hotkey('shift','enter')
-    pag.write("RA1811003020451", interval=0.1)
+    pag.write(regNo, interval=0.1)
     time.sleep(0.5)
     pag.hotkey('shift','enter')
-    pag.write("CSE 3-G", interval=0.1)
+    pag.write(classSec, interval=0.1)
     pag.press('enter')
 
 
 
 if __name__ == "__main__":
     meeting_join()
-    comment()
+    if(foo):
+        comment(name,regNo,classSec)
     time.sleep(meeting_time*60)
     pag.hotkey('ctrl','w')
     print('Meeting ended')
